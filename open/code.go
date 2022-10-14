f1ecf8b530542e53af036f765480c25ccd97cee2
pengshang <786188095@qq.com>
2022-10-14 15:52:18 +0800

    快速注册小程序提交

--- open/code.go
+++ open/code.go
@@ -154,7 +154,7 @@ func (m *MiniPrograms) ApplyPrivacyInterface() (err error) {
 		"http://tcpublic-1254389369.cos.ap-guangzhou.myqcloud.com/fem/resource/da13e4aa33a6f15de4c5572841ec9bdf.jpg",
 	}
 	applyPrivacyInterfaceParams := ApplyPrivacyInterfaceParam{
-		ApiName: "wx.getLocation",
+		ApiName: "wx.getFuzzyLocation",
 		Content: "由于涉及求职招聘业务，需要使用用户模糊的经纬度数据，在系统内用于精确解析用户所在城市推荐职位。",
 		PicList: picList,
 	}
